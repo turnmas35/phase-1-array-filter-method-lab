783c63b110042ab3c4b44c47284c56bc9c9c766f
Matthew Turner <turnmas35@gmail.com>
2023-10-21 14:45:32 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+  }
+
+  function matchName(driverObjects, query) {
+    return driverObjects.filter(driver => driver.name === query);
+  }
+  
